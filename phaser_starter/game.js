51caa767f2345d7832e425cc2d2ff51505619207
pfaustino <pfaustino@users.noreply.github.com>
2026-01-03 16:58:36 -0800

    Update .cursorrules with versioning policy

--- phaser_starter/game.js
+++ phaser_starter/game.js
@@ -3059,7 +3059,7 @@ function create() {
         .setDepth(30000);
 
     // Version Number
-    this.add.text(this.scale.width - 10, 10, 'v0.9.183', {
+    this.add.text(this.scale.width - 10, 10, 'v0.9.184', {
         fontFamily: 'Arial',
         fontSize: '16px',
         color: '#ffffff',
